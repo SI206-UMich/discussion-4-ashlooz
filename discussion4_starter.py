09e6a3987ec727fd78df870e8977d98ecdd4d0bf
ashlooz <97359169+ashlooz@users.noreply.github.com>
2023-12-05 14:23:33 -0500

    Update discussion4_starter.py

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -5,46 +5,42 @@ class Rectangle():
     # It sets an instance variable, "width" to the passed argument, width
     # It sets an instance variable, "height" to the passed argument, height
 
-    # YOUR CODE HERE
-
-
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     # Create the "__str__" method
     #
     # It returns a string, 
     #       "A rectangle with width ____ and height ____"
-
-    # YOUR CODE HERE
-
-
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
     # Create the "verify_input" method
     #
     # It returns a boolean
     #       True if the width and height are positive numbers
     #       False otherwise
-
-    # YOUR CODE HERE
-
-
+    def verify_input(self):
+        return self.width > 0 and self.height > 0
 
     # Create the "area" method
     #
     # It first verifies inputs and return "Invalid input" if they are invalid.
     # Otherwise, it returns the area of the rectangle.
-
-    # YOUR CODE HERE
-
-
+    def area(self):
+        if not self.verify_input():
+            return "Invalid input"
+        return self.width * self.height
 
     # Create the "perimeter" method
     #
     # It first verifies inputs and return "Invalid input" if they are invalid.
     # Otherwise, it returns the perimeter of the rectangle.
-
-    # YOUR CODE HERE
-    
-
+    def perimeter(self):
+        if not self.verify_input():
+            return "Invalid input"
+        return 2 * (self.width + self.height)
 
 def main():
     r = Rectangle(10, 10)
